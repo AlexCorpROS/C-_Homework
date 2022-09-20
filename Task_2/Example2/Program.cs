13669e35294c0e30ed972f51f7fb8c87606940fd
AlexCorpROS <arcorprintr@gmail.com>
2022-09-20 18:27:45 +0300

    add Task2_ex2

---
+++ Task_2/Example2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int getNumbertothreedigit(int number)
+{
+    int result = new int();
+    while(number > 1000 | number < -1000 )
+    {
+        number /= 10;
+    }
+    result = number % 10;
+    if(result < 0)
+    {
+        result *= -1;
+    }    
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100 && number > -100)
+Console.WriteLine("Третьей цифры нет");
+else
+{
+    int num = getNumbertothreedigit(number);
+    Console.WriteLine(num);    
+}
