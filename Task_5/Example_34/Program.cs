d84cb44fe33886973346a636b0f36df1a0ac2afd
AlexCorpROS <arcorprintr@gmail.com>
2022-10-03 18:21:38 +0300

    add Task_5

---
+++ Task_5/Example_34/Program.cs
@@ -0,0 +1,105 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] getRandomArrey(int UserNumber)
+{
+    int[] UserArray = new int [UserNumber];
+    var rnd = new Random();
+    Console.WriteLine($"Полученный рандомный массив трехзначных чисел длинной {UserNumber}");
+    for(int i = 0 ; i < UserNumber ; i++)
+    {
+        UserArray[i] = rnd.Next(100, 1000);
+        Console.Write($"{UserArray[i]} ");
+    }
+    Console.WriteLine("");
+    return UserArray;
+}
+
+int getCountPositivNumbers(int[] UserArrey)
+{
+    int count = 0;
+    for (int i = 0; i < UserArrey.Length; i++)
+    {
+        if(UserArrey[i] % 2 == 0)
+        {
+            count++;
+        }        
+    }    
+    return count;
+}
+
+Console.WriteLine("Введите длину массива : ");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+int[] RR = getRandomArrey(UserNumber);
+int CountPositiv = getCountPositivNumbers(RR);
+Console.WriteLine($"В этом массиве {CountPositiv} четных числа");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+void getPrint(string Text, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(Text);
+    Console.ResetColor();
+}
+int[] getTestToCorrect(string Number,string St )
+{
+    int[] userNumbers = new int[2];
+    int resultA = 0;
+    int resultB = -1;
+    while (resultA == 0 && resultB < 0)
+    {
+        Console.Write("Введите число которое хотите возвести в степень : ");        
+        string number = Console.ReadLine();
+        int.TryParse(number,out resultA);
+        
+        Console.Write("Введите степень в которую хотите возвести число : ");        
+        string st = Console.ReadLine();
+        int.TryParse(st,out resultB);
+        if(resultA == 0 && resultB < 0) 
+        {
+            getPrint($"Измените один из параметров. На ноль делить нельзя ", ConsoleColor.Red);
+            Console.WriteLine("");
+        }
+        else
+        {
+            break;
+        }
+    }
+    userNumbers[0] = resultA;
+    userNumbers[1] = resultB;
+    return userNumbers;
+}
+
+int[] Arr = getTestToCorrect("Введите число А:"," Введите число B: ");
+double answer = 1;
+for(int i = 1; i <= Arr[1] ; i++)
+{
+    answer = answer * Arr[0] ;
+}
+Console.WriteLine("");
+getPrint($"Число { Arr[0] } возведенное в степень {Arr[1]}  равно {answer} ", ConsoleColor.Cyan);
+*/
+
---
+++ Task_5/Example_36/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] getRandomArrey(int Number, int min , int max)
+{
+    int[] ResultArray = new int [Number];
+    var rnd = new Random();
+    Console.WriteLine($"Полученный рандомный массив чисел диапазона от {min} до {max} длинной {Number}");
+    for(int i = 0 ; i < Number ; i++)
+    {
+        ResultArray[i] = rnd.Next(min, max + 1);
+        Console.Write($"{ResultArray[i]} ");
+    }
+    Console.WriteLine("");
+    return ResultArray;
+}
+
+int getSumOddPositionElements(int[] UserArrey)
+{
+    int Sum = 0;
+    for (int i = 1; i < UserArrey.Length; i+= 2)
+    {
+        Sum += UserArrey[i];
+    }
+    return Sum;
+}
+
+Console.WriteLine("Введите длину массива : ");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальнное знначение элементов массива :");
+int Usermin = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальнное знначение элементов массива :");
+int Usermax = Convert.ToInt32(Console.ReadLine());
+
+int[] UserArrey = getRandomArrey( UserNumber, Usermin , Usermax );
+int Summa = getSumOddPositionElements(UserArrey);
+Console.WriteLine($"Сумма нечетных элементов рандомного массива длиной {UserNumber} элемента равна {Summa}");
